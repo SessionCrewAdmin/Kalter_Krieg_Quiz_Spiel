b61667877ff303d3eeee3d118d2e804725ccb638
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-18 16:41:18 +0200

    Refresh PWA cache after navigation hotfix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-v21-20260918-4';
+const CACHE='kathleen-v21-20260918-5';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html',
