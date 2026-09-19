665dcb4526a551f4c886588796e014bd29c41468
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-19 13:53:12 +0200

    Refresh cache for live Classroom Supabase controls

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-classroom-board-v22-20260919';
+const CACHE='kathleen-classroom-realtime-20260919';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
