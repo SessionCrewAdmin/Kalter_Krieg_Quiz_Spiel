38bb4132a4bf6e12277079c675aad080d00a48fb
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-19 13:11:36 +0200

    Refresh cache for encrypted class list sync

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-voucher-reveal-20260919b';
+const CACHE='kathleen-class-cloud-20260919';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
