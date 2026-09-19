0360593b913d1139cbfb77cf808d8b66d4afe200
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-19 15:44:29 +0200

    Refresh Classroom Board V9 cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-board-toolbar-v8-20260919';
+const CACHE='kathleen-board-textnote-v9-20260919';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
