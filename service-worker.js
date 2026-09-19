de763cc8e3ec73646d79671e2c56f3c83721c960
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-19 13:59:49 +0200

    Refresh cache for Classroom V22.2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-classroom-realtime-20260919';
+const CACHE='kathleen-classroom-v22-2-20260919';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
