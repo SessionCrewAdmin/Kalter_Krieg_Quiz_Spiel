b4e89584f389d2363e0a44040a63376d9831454e
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-19 15:05:05 +0200

    Refresh cache for restored Classroom Board

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-board-canvas-freeze-fix-20260919';
+const CACHE='kathleen-board-stable-restore-20260919';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
