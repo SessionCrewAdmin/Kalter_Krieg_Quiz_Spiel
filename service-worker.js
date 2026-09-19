4ac3f3a727074b5cb024fe5f97b4bb37f4ef36ce
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-19 15:30:33 +0200

    Force refresh Classroom selector repair

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-board-selection-toolbar-v5-20260919';
+const CACHE='kathleen-board-selector-repair-v6-20260919';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
