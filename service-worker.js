83972641f616011e87b27c84bc92d89d18e38aa0
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-19 14:11:29 +0200

    Force Classroom Board hotfix cache refresh

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-board-performance-v2-20260919';
+const CACHE='kathleen-board-hotfix-20260919b';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
