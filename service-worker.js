61bc06a08db26eddba598626ff3977fc93678027
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-19 12:38:57 +0200

    Refresh cache after voucher picker fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-v21-5-secure-class-vault-20260918';
+const CACHE='kathleen-v21-5-voucher-classlist-fix-20260919';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
