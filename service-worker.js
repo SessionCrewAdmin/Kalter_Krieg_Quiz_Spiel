23913c52ca8711112905557aaff7eb2c126c1361
SessionCrewAdmin <jonach54@googlemail.com>
2026-09-19 14:25:10 +0200

    Force refresh expanded Classroom widgets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kathleen-board-widgets-v1-20260919';
+const CACHE='kathleen-widgets-expanded-20260919b';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './tools/english-world-quiz/index.html','./tools/english-world-quiz/bonus.html','./tools/homework-vouchers/index.html',
